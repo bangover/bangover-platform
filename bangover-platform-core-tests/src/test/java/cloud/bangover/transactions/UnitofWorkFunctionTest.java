b1170984a77a668a4ebec068964fd52547580bac
Dmitry Mikhaylenko <dmikhaylenko@hotmail.com>
2023-02-07 16:15:14 +0400

    Add unit-of-work abstraction for business functions

---
+++ bangover-platform-core-tests/src/test/java/cloud/bangover/transactions/UnitofWorkFunctionTest.java
@@ -0,0 +1,64 @@
+package cloud.bangover.transactions;
+
+import cloud.bangover.async.promises.Promises;
+import cloud.bangover.async.timer.Timeout;
+import cloud.bangover.functions.BusinessFunction;
+import cloud.bangover.functions.MockFunctionRunner;
+import cloud.bangover.functions.StubBusinessFunction;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UnitofWorkFunctionTest {
+  private static final RuntimeException EXCEPTION = new RuntimeException();
+
+  @Test
+  public void shouldBeAbortedAfterReplying() throws Exception {
+    // Given
+    BusinessFunction<Object, Object> originalFunction =
+        new StubBusinessFunction<>(Promises.resolvedBy(new Object()), Timeout.ofSeconds(3L));
+    MockUnitOfWorkFunction<Object, Object> uowFunction =
+        new MockUnitOfWorkFunction<>(originalFunction);
+    // When
+    MockFunctionRunner.createFor(uowFunction).executeFunction(new Object()).await();
+    // Then
+    Assert.assertTrue(uowFunction.isCompleted());
+  }
+
+  @Test
+  public void shouldBeAbortedAfterException() throws Exception {
+    // Given
+    BusinessFunction<Object, Object> originalFunction = context -> {
+      throw EXCEPTION;
+    };
+    
+    MockUnitOfWorkFunction<Object, Object> uowFunction =
+        new MockUnitOfWorkFunction<>(originalFunction);
+    
+    // When
+    RuntimeException error = Assert.assertThrows(RuntimeException.class, () -> {
+      MockFunctionRunner.createFor(uowFunction).executeFunction(new Object()).get(10L);
+    });
+
+    // Then
+    Assert.assertTrue(uowFunction.isAborted());
+    Assert.assertSame(EXCEPTION, error);
+  }
+
+  @Test
+  public void shouldBeAbortedAfterRejecting() throws Exception {
+    // Given
+    BusinessFunction<Object, Object> originalFunction =
+        new StubBusinessFunction<>(Promises.rejectedBy(EXCEPTION), Timeout.ofSeconds(3L));
+    MockUnitOfWorkFunction<Object, Object> uowFunction =
+        new MockUnitOfWorkFunction<>(originalFunction);
+    
+    // When
+    MockFunctionRunner.createFor(uowFunction).executeFunction(new Object()).await();
+    
+    // Then
+    Assert.assertTrue(uowFunction.isAborted());
+  }
+}
